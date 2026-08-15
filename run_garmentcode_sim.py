b9e80208ba4a7bd4b946324446a102c5afd03702
Dirk Neuhäuser <dirk.neu3@gmail.com>
2026-08-15 18:08:10 +0100

    Replace hardcoded cluster paths with container paths

--- run_garmentcode_sim.py
+++ run_garmentcode_sim.py
@@ -5,7 +5,7 @@ import json
 from pathlib import Path
 
 # add the path of GarmentCode
-sys.path.insert(1, '/is/cluster/fast/sbian/github/GarmentCodeV2/')
+sys.path.insert(1, '/opt/garment_code/')
 from assets.garment_programs.meta_garment import MetaGarment
 from assets.bodies.body_params import BodyParameters
 
@@ -29,7 +29,7 @@ def run_simultion_warp(pattern_spec, sim_config, output_path, easy_texture_path)
         body_name='mean_all',    # 'f_smpl_average_A40'
         smpl_body=False,   # NOTE: depends on chosen body model
         add_timestamp=False,
-        system_path='/is/cluster/fast/sbian/github/GarmentCodeV2/system.json',
+        system_path='/opt/garment_code/system.json',
         easy_texture_path=easy_texture_path
     )
 
